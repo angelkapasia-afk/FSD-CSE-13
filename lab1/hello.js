36d6ee7a4cb821263dfcbd7b3195edc63bff7e2f
angel kapasia <angelkapasia@gmail.com>
2026-07-21 14:47:47 +0530

    add function

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+function sayHello (name){
+    console.log(`Welcome to ${name}`);
+}
+const sayHi = (name) => {
+console.log(`Welcome to ${name}`);
+};
+const findSum = (a,b) => a+b;
+
+function sum(a, b){
+    return a+b;
+}
+sayHi('Janvi Tanwar');
+sayHello('Angel Kapasia')
+
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
+console.log(`sum of 2 and 5 is ${findSum(2, 5)}`);
