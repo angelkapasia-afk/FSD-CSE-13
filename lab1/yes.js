620734bab98c270db34285ae56c6bfc320f1c02b
angel kapasia <angelkapasia@gmail.com>
2026-07-22 11:49:20 +0530

    roll no. in words

--- lab1/yes.js
+++ lab1/yes.js
@@ -1,24 +1,39 @@
 /*write a fxn to take number btw 0 to 9 and return in words.*/
-const numberToWord = (num) => {
-  const words = [
-    "Zero",
-    "One",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-  ];
 
-  if (num >= 0 && num <= 9) {
-    return words[num];
-  } else {
-    return "Invalid number";
+const numberToWord = (num) => {
+  switch (num) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+    default:
+      return "Invalid Number";
   }
 };
+//console.log(numberToWord(5));
+/*create another fxn that takes a number and show in words with the help of towords fxn*/
+const rollNum ="2503201000192";
+const digits = String(rollNum).split("");
+console.log(digits);
+let inWords ="";
 
-console.log(numberToWord(9));
-console.log(numberToWord(13));
+digits.forEach((d)=>{
+inWords += " " + numberToWord(Number(d));
+});
+console.log(inWords);
